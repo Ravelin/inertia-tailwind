aa57ba8e0554fe63e3d0f41db3bede3d23ef8cf4
David Murray <david@braingohurty.co.uk>
2019-07-22 19:42:38 +0100

    changes

--- resources/js/partials/Footer.vue
+++ resources/js/partials/Footer.vue
@@ -1,13 +1,13 @@
 <template>
   <footer class="flex flex-col mx-auto lg:px-40">
     <div class="flex">
-      <div class="w-1/3 bg-gray-500 h-12">
+      <div class="w-1/3 bg-gray-500 h-56">
         <p>col 1</p>
       </div>
-      <div class="w-1/3 bg-gray-400 h-12">
+      <div class="w-1/3 bg-gray-400 h-56">
         <p>col 2</p>
       </div>
-      <div class="w-1/3 bg-gray-500 h-12">
+      <div class="w-1/3 bg-gray-500 h-56">
         <p>col 3</p>
       </div>
     </div>
